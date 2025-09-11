4f4c8acf5e59740b9af97d087d033a0d4ea9ca37
CoffeeAddictGuy <richcrwo@gmail.com>
2025-09-11 15:55:54 +0300

    preparation for two solution

---
+++ leetcode/leetcode_c_cpp/easy/7.c
@@ -0,0 +1,18 @@
+/*
+7. Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. 
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <mylittlelib.h>
+
+int reverse(int x){
+    
+}
+
+int main() {
+    int num = 3;
+    return 0;
+}
