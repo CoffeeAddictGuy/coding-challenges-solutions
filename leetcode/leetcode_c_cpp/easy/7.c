a9918c2dc4bd969623eccb7ef1b003b4ecdb0926
CoffeeAddictGuy <richcrwo@gmail.com>
2025-09-17 15:10:13 +0300

    dont solved

--- leetcode/leetcode_c_cpp/easy/7.c
+++ leetcode/leetcode_c_cpp/easy/7.c
@@ -9,10 +9,32 @@ If reversing x causes the value to go outside the signed 32-bit integer range [-
 #include <mylittlelib.h>
 
 int reverse(int x){
+    if (x == 0)
+        return x;
+    int neg = 0;
+    if (x < 0) {
+        neg = 1;
+        x *= -1;
+    }
+    int res = 0;
+    int rad = getRadix(x)-1;
     
+    while (x != 0) {
+        int digit = x % 10;
+        if (res > INT_MAX/10 || (res == INT_MAX/10 && digit > 7)) return 0;
+        if (res < INT_MIN/10 || (res == INT_MIN/10 && digit < -8)) return 0;
+        res += digit * pow(10, rad);
+        x /= 10;
+        rad--;
+    }
+
+    if (neg != 0)
+        res *= -1;
+    return res;
 }
 
 int main() {
-    int num = 3;
+    int num = 1534236469;
+    printf("Res - %d", reverse(num));
     return 0;
 }
