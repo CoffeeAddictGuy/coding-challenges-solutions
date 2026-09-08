70344dfd4f1c6ea5e8999e019620ca80a3e75a46
CoffeeAddictGuy <richcrwo@gmail.com>
2026-09-08 13:10:08 +0300

    pow without struct

--- leetcode/leetcode_c_cpp/medium/50.c
+++ leetcode/leetcode_c_cpp/medium/50.c
@@ -55,7 +55,7 @@ struct binary d_to_b(long long n) {
   return result;
 }
 
-double myPow(double x, int n) {
+double good_myPow(double x, int n) {
   if (n == 0 || x == 1) {
     return 1;
   }
@@ -92,24 +92,75 @@ double myPow(double x, int n) {
   return result;
 }
 
-int main() {
-
-  printf("--------------\n");
-
-  struct binary b = d_to_b(2147483647);
-  for (int i = b.len; i >= 0; i--) {
-    printf("%c", b.num[i]);
+double myPow(double x, int n) {
+  if (n == 0 || x == 1) {
+    return 1;
   }
-  printf("\n");
 
-  printf("--------------\n");
+  double result = 1;
 
+  long steps = n;
+  bool mode = false;
+
+  if (steps < 0) {
+    mode = true;
+    steps *= -1;
+  }
+
+  if (steps == 1 && mode) {
+    return 1 / x;
+  } else if (steps == 1 && !mode) {
+    return x;
+  }
+
+  double z = x;
+  while (steps > 1) {
+    int t = steps % 2;
+    steps /= 2;
+    if (t == 0) {
+      z *= z;
+    } else {
+      result *= z;
+      z *= z;
+    }
+
+    if (steps == 1) {
+      t = 1;
+
+      if (t == 0) {
+        z *= z;
+      } else {
+        result *= z;
+        z *= z;
+      }
+    }
+  }
+
+  if (mode) {
+    result = 1 / result;
+  }
+
+  return result;
+}
+
+int main() {
   printf("OLD Result is %f\n", old_myPow(2, 10));
   printf("OLD Result is %f\n", old_myPow(2.1, 3));
   printf("OLD Result is %f\n", old_myPow(2, -2));
   printf("OLD Result is %f\n", old_myPow(8.84372, -5));
   printf("OLD Result is %f\n", old_myPow(8.95371, -1));
   printf("OLD Result is %f\n", old_myPow(2, -2147483648));
+
+  printf("--------------\n");
+
+  printf("GOOD Result is %f\n", good_myPow(2, 10));
+  printf("GOOD Result is %f\n", good_myPow(2.1, 3));
+  printf("GOOD Result is %f\n", good_myPow(2, -2));
+  printf("GOOD Result is %f\n", good_myPow(8.84372, -5));
+  printf("GOOD Result is %f\n", good_myPow(8.95371, -1));
+  printf("GOOD Result is %f\n", good_myPow(2, -2147483648));
+
+  printf("--------------\n");
 
   printf("Result is %f\n", myPow(2, 10));
   printf("Result is %f\n", myPow(2.1, 3));
